36331127afa70f217b642efcd9d1fc14ea5cdaf9
Aprytex <themaxrex9@gmail.com>
2022-01-23 19:25:49 -0600

    svg home

--- lib/DOWNBAR PAGES/HOME/mainhome.dart
+++ lib/DOWNBAR PAGES/HOME/mainhome.dart
@@ -1,9 +1,32 @@
+import 'package:app_patito/UTILIDADES/dimensiones.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class Home extends StatelessWidget {
   const Home() : super();
   @override
   Widget build(BuildContext context) {
-    return Text('hola soy la hoooom');
+    return Column(
+      children: [
+        Center(
+          child: Text(
+            'Bienvenido',
+            style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+          ),
+        ),
+        Container(
+          width: Dimensiones.bloqueAnchura * 90,
+          height: Dimensiones.bloqueAltura * 50,
+          child: SvgPicture.asset('./assets/chat.svg'),
+        ),
+        Center(
+          child: Text(
+            'Usuario1',
+            style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 12),
+          ),
+        ),
+        Center(child: Text('Proyecto asignado')),
+      ],
+    );
   }
 }
